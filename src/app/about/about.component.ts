cf22a555920d72608cb79e1ca05614e33c72117e
vinodh.gunasekaran <techie.vinodh@gmail.com>
2024-02-20 15:50:56 +0530

    api changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { response } from "express";
+import { Observable, fromEvent, interval } from "rxjs";
 
 @Component({
   selector: "about",
@@ -7,7 +9,36 @@ import { Component, OnInit } from "@angular/core";
 })
 export class AboutComponent implements OnInit {
   constructor() {}
+  //@ViewChild("btnSave") btnSave;
+  ngOnInit() {
+    const http$ = Observable.create((observer) => {
+      fetch('/api/courses')
+        .then((response) => {
+          return response.json();
+        })
+        .then((body) => {
+          observer.next(body);
+          observer.complete();
+        })
+        .catch((err) => observer.error(err));
+    });
 
-  ngOnInit() {}
+    http$.subscribe(courses => console.log(courses));
+  }
 
+  private simpleRxjsExample() {
+    const interval$ = interval(1000);
+    interval$.subscribe((val) => console.log("stream 1 => " + val));
+
+    const click$ = fromEvent(document, "click");
+    click$.subscribe((evt) => console.log(evt));
+
+    // document.addEventListener('click', evt => {
+    //   console.log(evt)
+    //   let counter =0;
+    //   setInterval(() => {console.log(counter); counter++}, 1000);
+    //   setTimeout(()=> console.log("completed"), 3000)
+
+    // });
+  }
 }
