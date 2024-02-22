01b29753017c4a8a76b2f2993f2dd32317137d6b
vinodh.gunasekaran <techie.vinodh@gmail.com>
2024-02-22 14:16:11 +0530

    Home page changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,19 @@
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { response } from "express";
-import { Observable, fromEvent, interval, noop } from "rxjs";
+import {
+  BehaviorSubject,
+  Observable,
+  Subject,
+  concat,
+  from,
+  fromEvent,
+  interval,
+  noop,
+  of,
+} from "rxjs";
 import { createHttpObservable } from "../common/util";
+import { error } from "console";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "about",
@@ -10,20 +22,60 @@ import { createHttpObservable } from "../common/util";
 })
 export class AboutComponent implements OnInit {
   constructor() {}
-  courses:any;
+  courses: any;
+  numbers$ = of(1, 2, 3);
   //@ViewChild("btnSave") btnSave;
   ngOnInit() {
-    const http$ = createHttpObservable('/api/courses');
-    //const courses$ 
-    http$.subscribe(
-      courses => {
-        console.log(courses);
+    // Create an observable sequence with values 1, 2, 3
+    const source$ = of(1, 2, 3);
+
+    // Create a new observable that emits the original values along with an additional value
+    const modified$ = new Observable<number>((subscriber) => {
+      source$.subscribe({
+        next: (value) => subscriber.next(value), // Pass original values
+        complete: () => {
+          subscriber.next(4); // Add a new value
+          subscriber.complete(); // Complete the sequence
+        },
+      });
+    });
+    // Subscribe to the modified observable to see the result
+    modified$.subscribe((value) => console.log(value)); // Output: [1, 2, 3, 4]
+
+    this.numbers$.subscribe({
+      next: (value) =>
+        console.log("Observable emitted the next value: " + value),
+      error: (err) => console.error("Observable emitted an error: " + err),
+      complete: () => {
+        console.log("Observable emitted the complete notification");
       },
-      noop,
-      () => console.log("completed")
-      );
+    });
+
+    // this.numbers$.next(1);
+    // this.numbers$.next(2);
+    // this.numbers$.next(3);
+
+    // this.numbers$.subscribe(
+    //   next => console.log(`Number value is: ${next}`),
+    //   error => console.error(error),
+    //   () => console.log('completed'));
+
+    //   this.numbers$.next(4)
+    //   this.numbers$.subscribe(
+    //     next => console.log(`Number value from second sub is: ${next}`),
+    //     error => console.error(error));
+    //   //() => console.log('second sub completed'));
+
+    //   this.numbers$.next(5);
+
+    //  const source1$ = of(1,2,3);
+    //  const source2$ = of(4,5,6);
+    //  const source3$ = of(6,7,8);
+
+    //  const result$ = concat(source1$, source2$, source3$);
+    //  result$.subscribe(console.log);
   }
-  
+
   private simpleRxjsExample() {
     const interval$ = interval(1000);
     interval$.subscribe((val) => console.log("stream 1 => " + val));
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,36 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-
+import { Component, OnInit } from "@angular/core";
+import { Course } from "../model/course";
+import { interval, Observable, of, timer } from "rxjs";
+import {
+  catchError,
+  delayWhen,
+  map,
+  retryWhen,
+  shareReplay,
+  tap,
+} from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  advancedCourses$: Observable<Course[]>;
+  beginnerCourses$: Observable<Course[]>;
 
+  ngOnInit() {
+    const http$: Observable<Course[]> = createHttpObservable(
+      "/api/courses"
+    ).pipe(map((res) => Object.values(res["payload"])));
 
-    constructor() {
+    this.beginnerCourses$ = http$.pipe(
+      map((courses) => courses.filter((p) => p.category == "BEGINNER"))
+    );
 
-    }
-
-    ngOnInit() {
-
-
-
-    }
-
+    this.advancedCourses$ = http$.pipe(
+      map((courses) => courses.filter((p) => p.category == "ADVANCED"))
+    );
+  }
 }
