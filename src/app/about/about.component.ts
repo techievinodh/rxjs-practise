65e3e6a131dca61ec7c415b38cbce8b1292fd2be
vinodh.gunasekaran <techie.vinodh@gmail.com>
2024-02-23 16:02:52 +0530

    Unsubsciption changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from "@angular/core";
 import { response } from "express";
 import {
   BehaviorSubject,
   Observable,
   Subject,
+  Subscription,
   concat,
   from,
   fromEvent,
@@ -13,82 +14,29 @@ import {
 } from "rxjs";
 import { createHttpObservable } from "../common/util";
 import { error } from "console";
-import { map } from "rxjs/operators";
+import { map, take, takeUntil, takeWhile } from "rxjs/operators";
+import { unsub } from "../common/unsub";
 
 @Component({
   selector: "about",
   templateUrl: "./about.component.html",
   styleUrls: ["./about.component.css"],
 })
-export class AboutComponent implements OnInit {
-  constructor() {}
+export class AboutComponent extends unsub implements OnInit { 
   courses: any;
   numbers$ = of(1, 2, 3);
+  data$ = interval(1000);
+  //dataSubscription: Subscription | undefined;
+ // unsubscribe$ = new Subject<void>();
   //@ViewChild("btnSave") btnSave;
   ngOnInit() {
-    // Create an observable sequence with values 1, 2, 3
-    const source$ = of(1, 2, 3);
-
-    // Create a new observable that emits the original values along with an additional value
-    const modified$ = new Observable<number>((subscriber) => {
-      source$.subscribe({
-        next: (value) => subscriber.next(value), // Pass original values
-        complete: () => {
-          subscriber.next(4); // Add a new value
-          subscriber.complete(); // Complete the sequence
-        },
-      });
-    });
-    // Subscribe to the modified observable to see the result
-    modified$.subscribe((value) => console.log(value)); // Output: [1, 2, 3, 4]
-
-    this.numbers$.subscribe({
-      next: (value) =>
-        console.log("Observable emitted the next value: " + value),
-      error: (err) => console.error("Observable emitted an error: " + err),
-      complete: () => {
-        console.log("Observable emitted the complete notification");
-      },
-    });
-
-    // this.numbers$.next(1);
-    // this.numbers$.next(2);
-    // this.numbers$.next(3);
-
-    // this.numbers$.subscribe(
-    //   next => console.log(`Number value is: ${next}`),
-    //   error => console.error(error),
-    //   () => console.log('completed'));
-
-    //   this.numbers$.next(4)
-    //   this.numbers$.subscribe(
-    //     next => console.log(`Number value from second sub is: ${next}`),
-    //     error => console.error(error));
-    //   //() => console.log('second sub completed'));
-
-    //   this.numbers$.next(5);
-
-    //  const source1$ = of(1,2,3);
-    //  const source2$ = of(4,5,6);
-    //  const source3$ = of(6,7,8);
-
-    //  const result$ = concat(source1$, source2$, source3$);
-    //  result$.subscribe(console.log);
+    this.data$.pipe(takeUntil(this.unsubscribe$)).subscribe((data)=> console.log('data', data));
   }
 
-  private simpleRxjsExample() {
-    const interval$ = interval(1000);
-    interval$.subscribe((val) => console.log("stream 1 => " + val));
+  // ngOnDestroy(): void {
+  //   //this.dataSubscription?.unsubscribe();
+  //   this.unsubscribe$?.next();
+  //   this.unsubscribe$?.complete();
+  // }
 
-    const click$ = fromEvent(document, "click");
-    click$.subscribe((evt) => console.log(evt));
-
-    // document.addEventListener('click', evt => {
-    //   console.log(evt)
-    //   let counter =0;
-    //   setInterval(() => {console.log(counter); counter++}, 1000);
-    //   setTimeout(()=> console.log("completed"), 3000)
-
-    // });
-  }
 }
